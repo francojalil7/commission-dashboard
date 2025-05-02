06946c9fef79631956e8de3fd1acce8044874dbc
francojalil <franco.jalil@zentricx.com>
2025-05-02 14:31:42 -0300

    feaT: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Commission Dashboard",
+  icons: {
+    icon: "/favicon.ico",
+  },
   description:
     "Unified commission dashboard that parses, validates and standardizes sales data from multiple CRMs to calculate individual and total commissions in a clear, extensible UI.",
 };
@@ -11,6 +14,9 @@ export const metadata: Metadata = {
 export default async function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en">
+      <head>
+        <link href="/favicon.ico" rel="icon" sizes="any" />
+      </head>
       <body className="container m-auto grid min-h-screen grid-rows-[auto_1fr_auto] gap-8 p-4 font-sans antialiased">
         {children}
       </body>
